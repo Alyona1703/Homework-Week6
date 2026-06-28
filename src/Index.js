3f684e39bef7716da0265b58572d8176c0bebde9
Alyona1703 <lyonathocv2@gmail.com>
2026-06-28 21:19:40 +0200

    Made the correct order in the code

--- src/Index.js
+++ src/Index.js
@@ -2,15 +2,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector(#description);
+  let humidityElement = document.querySelector(#humidity);
+  let windSpeedElement = document.querySelector(#wind-speed);
+  windSpeedElement.innerHTML = `&{response.data.wind.speed} km/h`;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
-  let descriptionElement = document.querySelector(#description);
-    descriptionElement.innerHTML = response.data.condition.description;
-  let humidityElement = document.querySelector(#humidity);
+  descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `&{response.data.temperature.humidity}%`;
-    let windSpeedElement = document.querySelector(#wind-speed);
-  windSpeedElement.innerHTML = `&{response.data.wind.speed} km/h`;
-
 }
 
 function search(event) {
